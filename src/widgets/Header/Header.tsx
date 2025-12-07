d3d0150be584389a7894f63229eb395a1932c868
Michael Savelev <m.savelev@emcd.io>
2025-12-07 11:07:05 +0300

    Add Header mobile

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
-import { Container } from '@shared/ui/Container/Container';
+import { Container, Text } from '@shared/ui';
 import logo from '@assets/logo.png';
-
+import { slide as Menu } from 'react-burger-menu';
 import { whiteHeaderPages } from '@shared/constants/darkHeaderPages';
-import '@widgets/Header/Header.scss';
 import { navigationLinks } from '@shared/constants/navigationLinks';
+import cn from 'classnames';
+import PhoneIcon from '@assets/icons/phone.svg';
+import '@widgets/Header/Header.scss';
 
 export const Header = () => {
+  const [isOpenBurgerMenu, setIsOpenBurgerMenu] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
   const { pathname } = useLocation();
   const [theme, setTheme] = useState<'light' | 'dark'>('light');
@@ -33,23 +36,31 @@ export const Header = () => {
     }
   }, [pathname]);
 
+  useEffect(() => {
+    if (isOpenBurgerMenu) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }, [isOpenBurgerMenu]);
+
   return (
     <header
-      className={`header${
-        isScrolled ? ' header--scrolled' : ''
-      } header__${theme}`}
+      className={cn('header', `header--${theme}`, {
+        'header--scrolled': isScrolled,
+      })}
       role="banner"
     >
       <Container>
         <div className="header__inner">
           <a
             href="/"
-            className="topbar__logo"
+            className="header__logo"
             aria-label="Свадебное агентство «Счастье»"
           >
             <img
               src={logo}
-              className="topbar__logo-image"
+              className="header__logo-image"
               alt="Свадебное агентство «Счастье»"
             />
           </a>
@@ -61,17 +72,46 @@ export const Header = () => {
                 to={to}
                 end={end}
                 className={({ isActive }) =>
-                  `nav-link${isActive ? ' is-active' : ''}`
+                  cn('navbar__link', { 'navbar__link--active': isActive })
                 }
               >
                 {label}
               </NavLink>
             ))}
           </div>
+          <Text
+            variant="body1"
+            color="pink"
+            textTransform="uppercase"
+            weight="bold"
+            className="header__slogan"
+          >
+            Свадебное агентство «Счастье»
+          </Text>
 
-          <div className="topbar__contacts">
-            <a className="contact-link" href="tel:+79372899055">
-              +7&nbsp;(937)&nbsp;289-90-55
+          <Menu
+            right
+            className="navbar__burger-menu"
+            isOpen={isOpenBurgerMenu}
+            onStateChange={({ isOpen }) => setIsOpenBurgerMenu(isOpen)}
+          >
+            {navigationLinks.map(({ to, label, end }) => (
+              <NavLink
+                key={to}
+                to={to}
+                end={end}
+                className="navbar__burger-menu__item"
+                onClick={() => setIsOpenBurgerMenu(false)}
+              >
+                {label}
+              </NavLink>
+            ))}
+          </Menu>
+
+          <div className="header__contacts">
+            <a href="tel:+79372899055">
+              <span className="contact-link">+7&nbsp;(937)&nbsp;289-90-55</span>
+              <img src={PhoneIcon} alt="Телефон" className="contact-icon" />
             </a>
             <span className="contact-text">Свадебное агентство г.Казань</span>
           </div>
