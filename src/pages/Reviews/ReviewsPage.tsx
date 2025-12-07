c1c3d74e948e7c738579b0e007a45f9c3b5e3592
Michael Savelev <m.savelev@emcd.io>
2025-12-07 09:04:52 +0300

    Update circles

--- src/pages/Reviews/ReviewsPage.tsx
+++ src/pages/Reviews/ReviewsPage.tsx
@@ -1,11 +1,9 @@
 import { PageHeader } from '@widgets/PageHeader/PageHeader';
 import { Container } from '@shared/ui/Container/Container';
-import { Text } from '@shared/ui/Text/Text';
 
 import {
   REVIEWS_ITEMS,
   REVIEWS_PAGE_BOTTOM_TEXT,
-  REVIEWS_PAGE_SUBTITLE,
   REVIEWS_PAGE_TITLE,
 } from '@shared/constants';
 
@@ -26,27 +24,6 @@ export const ReviewsPage = () => {
 
       <section className="reviews">
         <Container>
-          <header className="reviews__intro">
-            <Text
-              variant="h3"
-              font="body"
-              weight="semibold"
-              align="center"
-              textTransform="uppercase"
-              className="reviews__title"
-            >
-              {REVIEWS_PAGE_TITLE}
-            </Text>
-            <Text
-              variant="body2"
-              font="helvetica"
-              align="center"
-              className="reviews__subtitle"
-            >
-              {REVIEWS_PAGE_SUBTITLE}
-            </Text>
-          </header>
-
           <div className="reviews__list">
             {REVIEWS_ITEMS.map((item) => (
               <ReviewCard key={item.id} item={item} />
